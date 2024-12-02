e4cd17d1331dc16a5c05005820879a8c334f1934
joelfright <jwf520@york.ac.uk>
2024-12-02 00:11:53 +0000

    feat: part two and readme

--- day1/solution.js
+++ day1/solution.js
@@ -3,23 +3,35 @@ import readInput from '../helper.js';
 export function solution() {
   const input = readInput(1).split('\n');
 
-  // Format the lists
   let list = { one: [], two: [] }
-  input.forEach((pair) => {
+  input.forEach(pair => {
     const number = pair.split('   ');
     list.one.push(Number(number[0]));
     list.two.push(Number(number[1]));
   });
 
-  // Sort the lists
   list.one.sort();
   list.two.sort();
 
-  // Calculate differences
-  let result = 0;
-  for (let index = 0; index < list.one.length; index++) {
-    result += Math.abs(list.one.at(index) - list.two.at(index));
+  function partOne(){
+    let result = 0;
+    for (let index = 0; index < list.one.length; index++) {
+      result += Math.abs(list.one.at(index) - list.two.at(index));
+    }
+    return result;
   }
 
-  return result;
+  function partTwo(){
+    let countedNum = {};
+    list.two.forEach(value => {
+        countedNum[value] = (countedNum[value] || 0) + 1;
+    })
+    let result = 0;
+    list.one.forEach(number => {
+      result += number * (countedNum[number] ? countedNum[number] : 0)
+    })
+    return result;
+  }
+
+  return `Part 1: ${partOne()} / Part 2: ${partTwo()}`;
 }
