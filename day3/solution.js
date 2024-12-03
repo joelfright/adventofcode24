dc5e5de3a43daa31993864519fb21b6d87dbae83
joelfright <jwf520@york.ac.uk>
2024-12-03 17:01:58 +0000

    feat: day three solution

---
+++ day3/solution.js
@@ -0,0 +1,28 @@
+import readInput from '../helper.js';
+
+export function solution() {
+  const input = readInput(3);
+  let resultP1 = 0;
+  let resultP2 = 0;
+
+  const arr = input.match(/mul\([0-9]+\,[0-9]+\)/gm);
+  arr.forEach(command => {
+    let [num1, num2] = command.slice(4, -1).split(',').map(Number);
+    resultP1 += num1 * num2;
+  })
+
+  const arr2 = input.match(/(do\(\)|don't\(\)|mul\([0-9]+\,[0-9]+\))/gm);
+  let enabled = true;
+  arr2.forEach(command => {
+    if (command === 'do()') {
+      enabled = true;
+    } else if (command === "don't()") {
+      enabled = false;
+    } else if (enabled && command.startsWith('mul(')) {
+      let [num1, num2] = command.slice(4, -1).split(',').map(Number);
+      resultP2 += num1 * num2;
+    }
+  })
+
+  return `Part 1: ${resultP1} / Part 2: ${resultP2}`;
+}7
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { solution as day1 } from './day1/solution.js';
 import { solution as day2 } from './day2/solution.js';
+import { solution as day3 } from './day3/solution.js';
 
 console.log(`Day 1 | Answer: ${day1()}`);
 console.log(`Day 2 | Answer: ${day2()}`);
+console.log(`Day 2 | Answer: ${day3()}`);
